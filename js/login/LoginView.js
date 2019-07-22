70faf67da4e560229c3848d837eabbdb44e86037
Luis Ruiz <roco65@gmail.com>
2019-07-22 23:56:19 +0200

    eye test

--- js/login/LoginView.js
+++ js/login/LoginView.js
@@ -14,6 +14,30 @@
             {
                 var self = this;
 
+                $(".eye").one("load",function()
+                {
+                    let svgDoc = this.contentDocument;
+                    
+                    let eyeBall = svgDoc.querySelector(".eyeball"),
+                    pupil = svgDoc.querySelector(".pupil"),
+                    eyeArea = eyeBall.getBoundingClientRect(),
+                    pupilArea = pupil.getBoundingClientRect(),
+                    R = eyeArea.width/2,
+                    r = pupilArea.width/2,
+                    centerX = eyeArea.left + R,
+                    centerY = eyeArea.top + R;
+
+                    document.addEventListener("mousemove", (e)=>{
+                      let x = e.clientX - centerX,
+                          y = e.clientY - centerY,
+                          theta = Math.atan2(y,x),
+                          angle = theta*180/Math.PI + 360;
+                          
+                      pupil.style.transform = `translateX(${R - r +"px"}) rotate(${angle + "deg"})`;
+                      pupil.style.transformOrigin = `${r +"px"} center`;
+                    });
+                })
+                
                 $(document).ready(function ()
                 {             
                     $("#login")[0].showModal();
@@ -27,7 +51,7 @@
                     
                     componentHandler.upgradeAllRegistered();
                     
-                    self.presenter.checkToken();
+                    //self.presenter.checkToken();
                 });
             },
             enumerable: false
