02906ad1c2d91fac88d09f3b0a7963ad58b61db1
Luis Ruiz <roco65@gmail.com>
2019-07-23 00:33:17 +0200

    logo with moving eyes

--- js/login/LoginView.js
+++ js/login/LoginView.js
@@ -14,7 +14,7 @@
             {
                 var self = this;
 
-                $(".eye").one("load",function()
+                $("#login object").one("load",function()
                 {
                     let svgDoc = this.contentDocument;
                     
@@ -50,6 +50,48 @@
                     });
                     
                     componentHandler.upgradeAllRegistered();
+                    
+                    var canvas = $(".canvas")[0], context = canvas.getContext("2d");
+                  
+                    var size = 9;
+
+                    var rect = canvas.getBoundingClientRect();
+                    canvas.width = size * 2 + 15;
+                    canvas.height = size + 10;
+
+                    onMouseMove({clientX: 0, clientY: 0});
+                  
+                    document.addEventListener("mousemove", onMouseMove);
+
+                    function onMouseMove(event)
+                    {
+                        var x = event.clientX - rect.left,
+                            y = event.clientY - rect.top;
+                            
+                        context.clearRect(0, 0, size * 2 + 20, size + 20);
+
+                        drawEye(x, y, size / 2 + 5, size / 2 + 5);
+                        drawEye(x, y, size * 2.5 + 5, size / 2 + 5);
+                    }
+                  
+                    function drawEye(x, y, cx, cy)
+                    {
+                        var dx = x - cx,
+                            dy = y - cy,
+                            angle = Math.atan2(dy, dx);
+                            
+                        context.save();
+                        context.fillStyle = "#FFFFFF";
+                        context.translate(cx, cy);
+                        context.rotate(angle);
+                        // context.beginPath();
+                        // context.arc(0, 0, size / 2, 0, Math.PI * 2);
+                        // context.stroke();
+                        context.beginPath();
+                        context.arc(size * 0.4, 0, size * 0.17, 0, Math.PI * 2);
+                        context.fill();
+                        context.restore();
+                    }
                     
                     //self.presenter.checkToken();
                 });
